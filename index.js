2f3a8cda4d3e1ac8fdf33999545692c84b081f99
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-07-30 17:33:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 // Code your solution here
 function findMatching(drivers, string){
   let matching = drivers.filter(name => {
-    return name.toUpperCase() == string.toUpperCase()
+    return name.toUpperCase() === string.toUpperCase()
   })
   return matching;
+};
+function fuzzyMatch(drivers, string){
+  let letters = string.split('');
+
+  function filterByFirstLetter(drivers, letter){
+    let matchingDrivers = drivers.filter(name => {
+      let firstLetter = name.split("")[0];
+      return firstLetter.toUpperCase() === letter.toUpperCase()
+    })
+    return matchingDrivers;
+  };
 };
